9e8c274fb929db608e9fb8f6149e09fdd9ba8c3e
pedromtelho <pedropauloficial@hotmail.com>
2019-07-22 13:32:40 -0300

    Fixes 1

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, render_template
+# from QRreader import *
+import requests
+from datetime import datetime
+
+app = Flask(__name__)
+url = "https://www.btgpactual.com/btgcode/api/money-movement"
+
+@app.route('/')
+def mainpage():
+    balance = 10000
+    daySpent = 0
+    monthTransact = 0
+    day = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    templateData = {
+			'balance' : balance,
+            'daySpent': daySpent,
+            'monthTransact' : monthTransact
+    }
+    return render_template('home.html', results=templateData)
+
+
+# @app.route('/extract', methods=['GET'])
+# def Extract():
+
+#     resultDesc = []
+#     resultAmount = []
+#     resultCPart = []
+#     resultData = []
+
+#     headers = {
+#     "x-api-key": "6j7A1fN7buPWINRRD4HY9yo5SnqKAyeUn5W9ZDwq"
+#     }
+#     r = requests.get('https://www.btgpactual.com/btgcode/api/money-movement', headers=headers)
+#     listJson = r.json()
+
+
+#     for j in listJson:
+#         for i, k in j.items():
+#             if i == "Desc":
+#                 resultDesc.append(str(k))
+#             if i == "Amount":
+#                 resultAmount.append(str(k))
+#             if i == "Counterpart":
+#                 resultCPart.append(str(k))
+#             if i == "CreatedAt":
+#                 resultData.append(str(k[8:10] + k[4:8] + k[0:4]))
+#         extract = {
+#             'wallet' : resultCPart,
+# 			'desc' : resultDesc,
+#             'data': resultData,
+#             'value' : resultAmount
+#         }
+
+#     return render_template('extract.html', extract=extract)
+
+@app.route('/boleto')
+def Boleto():
+    return render_template('boleto.html')
+
+@app.route('/transact')
+def Transact():
+    return render_template('transact.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
