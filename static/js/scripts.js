47e5f56270166302ffe9027eef6134b803bdfd84
pedromtelho <pedropauloficial@hotmail.com>
2019-08-03 13:59:57 -0300

    PRONTO

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -29,7 +29,15 @@
 
     });
 
-
+    $(document).ready(function() {
+        $(chart_id).highcharts({
+            chart: chart,
+            title: title,
+            xAxis: xAxis,
+            yAxis: yAxis,
+            series: series
+        });
+    });
 
     /*  
     -----------------
